e992384e626ca70a9bd5fd8e2064f85e773d8067
unknown <danielrua78@gmail.com>
2025-02-08 15:25:58 -0500

    Reestructuración del proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Cargar el header
+    fetch("../components/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+
+            // Ahora que el header está cargado, cargamos el navigation dentro de él
+            fetch("../components/navigation.html")
+                .then(response => response.text())
+                .then(navData => {
+                    document.getElementById("navigation").innerHTML = navData;
+                });
+        });
+
+    // Cargar el footer
+    fetch("../components/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+});
+
--- scripts.js
+++
@@ -1,4 +0,0 @@
-function leerMas(id) {
-    const moreText = document.getElementById(`more-${id}`);
-    moreText.style.display = moreText.style.display === "none" || moreText.style.display === "" ? "block" : "none";
-}
